acf95d8836595186d42f048d88cbb0f9e74653b0
Bram Boer <btfboer@icloud.com>
2026-09-11 00:10:43 +0200

    build: add chat route

---
+++ src/routes/index.tsx
@@ -0,0 +1,4 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { ChatApp } from "@/components/chat/chat-app";
+
+export const Route = createFileRoute("/")({ component: ChatApp });
